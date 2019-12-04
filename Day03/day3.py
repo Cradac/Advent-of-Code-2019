c1e886283a78dce9b505c6a8cff14aa475ec452e
Cradac <39528837+Cradac@users.noreply.github.com>
2019-12-04 11:45:09 +0100

    Done did part 2 Day 3

--- Day03/day3.py
+++ Day03/day3.py
@@ -3,9 +3,25 @@ x_size = 25000
 y_size = 16000
 grid = [[0] * y_size for i in range(x_size)]
 
+one_steps, two_steps = 0, 0
+pointMap = {}
 
 intersectionList = []
 shortestDistance = 999999
+
+class Point:
+    def __init__(self, posX, posY, stepsOne):
+        self.posX = posX
+        self.posY = posY
+        self.stepsOne = stepsOne
+        self.stepsTwo = None
+        self.totalSteps = None
+    
+    def setStepsTwo(self, stepsTwo):
+        self.stepsTwo = stepsTwo
+        self.totalSteps = self.stepsOne + self.stepsTwo
+        return self
+
 
 # Read in Instructions
 f = open("Day3input.txt")
@@ -25,14 +41,18 @@ def goRight(steps: int, lineNo: int):
     for i in range(steps):
         x_pointer +=1
         grid[x_pointer][y_pointer] += lineNo
-        checkForIntersection()
+        # checkForIntersection()
+        makeStep(lineNo)
+
 
 def goLeft(steps: int, lineNo: int):
     global x_pointer, y_pointer
     for i in range(steps):
         x_pointer -=1
         grid[x_pointer][y_pointer] += lineNo
-        checkForIntersection()
+        # checkForIntersection()
+        makeStep(lineNo)
+
 
 def goUp(steps: int, lineNo: int):
     global x_pointer, y_pointer 
@@ -44,7 +64,9 @@ def goUp(steps: int, lineNo: int):
             print(x_pointer, y_pointer)
             exit(1)
 
-        checkForIntersection()
+        # checkForIntersection()
+        makeStep(lineNo)
+
         
 
 def goDown(steps: int, lineNo: int):
@@ -52,7 +74,26 @@ def goDown(steps: int, lineNo: int):
     for i in range(steps):
         y_pointer -=1
         grid[x_pointer][y_pointer] += lineNo
-        checkForIntersection()
+        # checkForIntersection()
+        makeStep(lineNo)
+
+def makeStep(lineNo):
+    global one_steps, two_steps, x_pointer, y_pointer, pointMap, intersectionList
+    if lineNo == 1:
+        one_steps += 1
+        try:
+            pointMap[(x_pointer, y_pointer)] = Point(x_pointer,y_pointer,one_steps)
+        except KeyError:
+            pass
+    elif lineNo == 2:
+        two_steps += 1
+        try:
+            if not pointMap[(x_pointer, y_pointer)].stepsTwo:
+                intersectionList.append(pointMap[(x_pointer, y_pointer)].setStepsTwo(two_steps))
+        except KeyError:
+            pass
+
+
 
 def checkForIntersection():
     global x_pointer, y_pointer, grid, intersectionList
@@ -86,8 +127,15 @@ execInstructions(line1Instructions, 1)
 setPointers()
 execInstructions(line2Instructions, 2)
 setPointers()
+
 for inter in intersectionList:
-    manhattenForumla(inter[0], inter[1])
+    if inter.totalSteps < shortestDistance and inter.totalSteps != 0:
+        shortestDistance = inter.totalSteps
+
+
+
+'''for inter in intersectionList:
+    manhattenForumla(inter[0], inter[1])'''
 
 print(f"Found {len(intersectionList)} crossings.")
 
